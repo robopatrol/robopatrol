82ab8241c8a70403ec1ed98c53be4361456b41fe
simmljoe <joel.simmler@gmail.com>
2016-03-07 14:52:30 +0100

    Add small test
    Hello World / add

---
+++ src/robopatrol/test/helloWorld/HelloWorldTest.py
@@ -0,0 +1,13 @@
+import unittest
+from HelloWorld import Greeter
+
+class MyTestCase(unittest.TestCase):
+    def test_default_greeting_set(self):
+        greeter = Greeter()
+        # this test will fail until you change the Greeter to return this expected message
+        self.assertEqual(greeter.message, 'Hello world_')
+        # this test will fail until you change the expected Value to 2
+        self.assertEqual(greeter.addOne(self, 1), 3)
+
+if __name__ == '__main__':
+    unittest.main()
