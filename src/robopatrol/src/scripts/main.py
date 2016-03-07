604650594474ca249511d1f9a7d59f6915634974
Marco Strobel <mail@strobel.ch>
2016-03-07 08:11:55 +0100

    update move forward/backward script:
    - Refactor as class
    - Works now with gazebo

--- src/robopatrol/src/scripts/main.py
+++ src/robopatrol/src/scripts/main.py
@@ -1,47 +1,57 @@
 #!/usr/bin/env python
 
 import rospy
-from std_msgs.msg import String
 from geometry_msgs.msg import Twist
 
 
+class RoboPatrol():
 
+    def __init__(self):
+        rospy.init_node('robopatrol_move')
+        
+    	rospy.loginfo("To stop RoboPatrol CTRL + C")
 
-def main():
-    print "Run robo patrol"
+        rospy.on_shutdown(self.shutdown)
 
-    rospy.init_node('robopatrol')
+        self.cmd_vel = rospy.Publisher('cmd_vel_mux/input/navi', Twist, queue_size=10)
+        
+        while not rospy.is_shutdown():
+            speed = input("Speed: ")
+            distance = input("Distance: ")
+            self.move(speed, distance)
 
-    while True:
-        speed = input("Speed: ")
-        distance = input("Distance: ")
-        move(speed, distance)
+    def move(self, speed, distance):
+        move_cmd = Twist()
+        
+        move_cmd.linear.x = speed
+        move_cmd.linear.y = 0
+        move_cmd.linear.z = 0
+
+        move_cmd.angular.x = 0
+        move_cmd.angular.y = 0
+        move_cmd.angular.z = 0
+
+        t0 = rospy.rostime.get_time()
+        current_distance = 0
+        rate = rospy.Rate(100);
+
+        while current_distance < distance:
+            self.cmd_vel.publish(move_cmd)
+            t1 = rospy.rostime.get_time()
+            current_distance = abs(speed * (t1 - t0))
+            rate.sleep()
+
+        self.cmd_vel.publish(Twist())
+
+        
+    def shutdown(self):
+        rospy.loginfo("Stop RoboPatrol")
+        self.cmd_vel.publish(Twist())
+        rospy.sleep(1)
     
-
-def move(speed, distance):
-    p = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
-    twist = Twist()
-    
-    twist.linear.x = speed
-    twist.linear.y = 0
-    twist.linear.z = 0
-
-    twist.angular.x = 0
-    twist.angular.y = 0
-    twist.angular.z = 0
-
-    t0 = rospy.rostime.get_time()
-    current_distance = 0
-
-    while current_distance < distance:
-        p.publish(twist)
-        t1 = rospy.rostime.get_time()
-        current_distance = abs(speed * (t1 - t0))
-
-    twist.linear.x = 0
-    p.publish(twist)
-
-
 
 if __name__ == "__main__":
-    main()
+    try:
+        RoboPatrol()
+    except:
+        rospy.loginfo("RoboPatrol terminated.")
