760048def016e5033ef8e764c7061e522fff5fb8
Rémi Georgiou <tonymontana303@gmail.com>
2016-04-20 13:07:26 +0200

    modified startRecording method -> fetch current username automatically

--- src/slam/createMap.py
+++ src/slam/createMap.py
@@ -11,13 +11,14 @@ class CreateMap():
     def __init__(self):
 	self.saveMap()
 
-
-    # @param : String bagfile (path + filename)
-    # simulates a patrol by playing back (= publishing messages) a rosbag file
-    # make sure you set the ROS parameter use_sim_time to TRUE before calling 'rosbag play'
-    # An instance of slam_mapping (package gmapping) must be active
-    # Normally, this is done when launching mapping.launch, but if you want to do it manually:
-    # rosrun gmapping slam_mapping
+    """
+    @param : String bagfile (path + filename)
+    simulates a patrol by playing back (= publishing messages) a rosbag file
+    make sure you set the ROS parameter use_sim_time to TRUE before calling 'rosbag play'
+    An instance of slam_mapping (package gmapping) must be active
+    Normally, this is done when launching mapping.launch, but if you want to do it manually:
+    rosrun gmapping slam_mapping
+    """
     def playBackRecording(self,bagfile):
 	if not bagfile == "":
 	    self.__setROSparams()
@@ -29,10 +30,12 @@ class CreateMap():
 	subprocess.call("rosparam set use_sim_time true", shell=True)
 
 
-    # An instance of slam_mapping (package gmapping) must be active before calling saveMap
-    # map_server service generates two files:
-    # - an image (THE MAP)
-    # - map.yaml (map meta data)
+    """
+    An instance of slam_mapping (package gmapping) must be active before calling saveMap
+    map_server service generates two files:
+    - an image (THE MAP)
+    - map.yaml (map meta data)
+    """
     def saveMap(self):
 	subprocess.call("rosrun map_server map_saver", shell=True)
 	
